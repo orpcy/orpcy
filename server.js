16e9dc86cce0ef04fe0b3e3046b58f94df79d907
Sofiyullah jamiu <sofiyullahj@gmail.com>
2020-03-17 05:33:36 +0100

    minor changes

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(port, () => console.log(`listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
